503257b9c81eff5a22d12276538bfcf69c717219
wibr7068 <71329264+wibr7068@users.noreply.github.com>
2024-11-14 17:03:48 +0100

    Changed dependencies and added mui chart

--- src/app/(pages)/constructors/page.tsx
+++ src/app/(pages)/constructors/page.tsx
@@ -1,16 +1,24 @@
 "use client"
 import { useEffect, useState } from "react";
+import { LineChart } from "@mui/x-charts";
+
+// Define the type for a constructor object
+interface ConstructorData {
+  year: number;
+  total_points: string | number; // Assuming total_points can be a string or number from API
+  [key: string]: any; // Allow other properties if necessary
+}
 
 export default function AboutPage() {
-  const [names, setNames] = useState([]);
+  const [constructors, setConstructors] = useState<ConstructorData[]>([]);
   const [year, setYear] = useState(2024);
   const [selectedYear, setSelectedYear] = useState(2024);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await window.fetch(`/api/constructors?year=${year}`);
-      const constructorNames = await response.json();
-      setNames(constructorNames);
+      const constructors = await response.json();
+      setConstructors(constructors);
     }
     fetchData();
   }, [year])
@@ -22,10 +30,25 @@ export default function AboutPage() {
       <input type="number" min={1950} max={2024} onChange={e => setSelectedYear(Number(e.target.value))}></input>
       <button onClick={(_) => setYear(selectedYear)}>Search</button>
       {
-        names.map((obj, i) => (
+        constructors.map((obj, i) => (
           <div key={i}>{JSON.stringify(obj)}<br /></div>
         ))
       }
+
+      <LineChart dataset={constructors.map(name => ({ ...name, total_points: Number(name.total_points) }))}
+        xAxis={[{ dataKey: "year" }]}
+        series={[
+          {
+            dataKey: "total_points",
+            
+          },
+        ]}
+        width={500}
+        height={300}
+      />
+
+      
+
     </div>
   );
 }
--- src/app/api/constructors/route.ts
+++ src/app/api/constructors/route.ts
@@ -7,7 +7,7 @@ export async function GET(req: NextRequest, res: NextResponse) {
     const url = new URL(req.url);
     const year = url.searchParams.get('year');
     //const items = await prisma.$queryRaw<{ id: string, name: string}[]>`SELECT id, name FROM constructor`;
-    const items = await prisma.$queryRaw<{ constructor_id: string}[]>`SELECT constructor_id, total_points FROM season_constructor WHERE year=${year}`;
+    const items = await prisma.$queryRaw<{ constructor_id: string}[]>`SELECT constructor_id, total_points, year FROM season_constructor WHERE year=${year}`;
 
     return new Response(JSON.stringify(items), {
       headers: { "Content-Type": "application/json" },
