391060c86c2d7ff9206431a0c57165c6c359720b
Markus Hellgren <markus.hellgren@gmail.com>
2024-11-14 13:57:55 +0100

    Moved database f1db file

--- src/utils/f1db.ts
+++ src/utils/f1db.ts
@@ -3,7 +3,7 @@ import { open } from "sqlite";
 
 export const openF1db = async () => {
 	return open({
-		filename: "./public/f1db.db",
+		filename: "./data/f1db.db",
 		driver: sqlite3.Database,
 	});
 }
