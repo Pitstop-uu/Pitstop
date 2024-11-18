e4b516f2f55d35f8b49f8752b308a50241605a61
wibr7068 <71329264+wibr7068@users.noreply.github.com>
2024-11-18 15:13:30 +0100

    Co-authored-by: Markus Hellgren <markhellg@users.noreply.github.com> Co-authored-by: Xergos0 <Xergos0@users.noreply.github.com>

--- src/app/(pages)/constructors/page.tsx
+++ src/app/(pages)/constructors/page.tsx
@@ -1,53 +1,110 @@
 "use client"
 import { useEffect, useState } from "react";
 import { LineChart } from "@mui/x-charts";
+import "@/styles/constructors.css";
+import { axisClasses } from '@mui/x-charts/ChartsAxis';
+import { AxisConfig } from '@mui/x-charts'
 
-// Define the type for a constructor object
-interface ConstructorData {
+interface ConstructorItem {
+  constructor_id: string;
   year: number;
-  total_points: string | number; // Assuming total_points can be a string or number from API
-  [key: string]: any; // Allow other properties if necessary
+  total_points: string | number;
+  [key: string]: any;
 }
+
+interface ConstructorData {
+  label: string;
+  data: number[];
+}
+
+type ConstructorDataPoint = {
+  year: number;
+  total_points: number;
+}
+
+type ConstructorRecord = Record<string, {
+  label: string;
+  data: ConstructorDataPoint[];
+}>
+
 
 export default function AboutPage() {
   const [constructors, setConstructors] = useState<ConstructorData[]>([]);
-  const [year, setYear] = useState(2024);
-  const [selectedYear, setSelectedYear] = useState(2024);
+  const [years, setYears] = useState([2023, 2024]);
+  const [firstYear, setFirstYear] = useState(2023);
+  const [lastYear, setLastYear] = useState(2024);
+  const [loading, setLoading] = useState(false);
+  const minYear = 1950;
+  const maxYear = 2024;
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await window.fetch(`/api/constructors?year=${year}`);
+      setLoading(true);
+      console.log('years', years);
+      const response = await window.fetch(`/api/constructors?year=${years}`);
       const constructors = await response.json();
-      setConstructors(constructors);
-    }
-    fetchData();
-  }, [year])
 
-  return (
-    <div>
-      <h1>Constructors</h1>
-      <p>This is the about page of our Next.js app.</p>
-      <input type="number" min={1950} max={2024} onChange={e => setSelectedYear(Number(e.target.value))}></input>
-      <button onClick={(_) => setYear(selectedYear)}>Search</button>
-      {
-        constructors.map((obj, i) => (
-          <div key={i}>{JSON.stringify(obj)}<br /></div>
-        ))
+      const constructorDataMap = constructors.reduce((acc: ConstructorRecord, constructor: ConstructorItem) => {
+        const { constructor_id, year, total_points } = constructor;
+        if (!acc[constructor_id]) {
+          acc[constructor_id] = { label: constructor_id, data: [] };
+        }
+
+        acc[constructor_id].data.push({ year, total_points: Number(total_points) });
+        acc[constructor_id].data.sort((a, b) => a.year - b.year);
+        return acc;
+      }, {} as ConstructorRecord);
+
+      const constructorSeries = [];
+      for (const key in constructorDataMap) {
+        const { label, data } = constructorDataMap[key];
+        constructorSeries.push(
+          { label, data: data.map((dataPoint: ConstructorDataPoint) => dataPoint.total_points) }
+        )
       }
 
-      <LineChart dataset={constructors.map(name => ({ ...name, total_points: Number(name.total_points) }))}
-        xAxis={[{ dataKey: "year" }]}
-        series={[
-          {
-            dataKey: "total_points",
-            
-          },
-        ]}
-        width={500}
-        height={300}
-      />
+      setConstructors(constructorSeries);
+      console.log(constructorSeries);
+      setLoading(false);
+    }
+    fetchData();
+  }, [years])
 
-      
+  
+
+  return (
+    <div className="container-constructors" style={{color: "white"}}>
+      <h1>Constructors</h1>
+      <p>This is the about page of our Next.js app.</p>
+      <input type="number" value={firstYear} min={minYear} max={maxYear} onChange={e => setFirstYear(Number(e.target.value))}></input>
+      <input type="number" value={lastYear} min={minYear} max={maxYear} onChange={e => setLastYear(Number(e.target.value))}></input>
+
+      <button onClick={(_) => {
+        setConstructors([]);
+        setYears([firstYear, lastYear]);
+      }}>Search</button>
+
+      {
+        !loading && (
+          <LineChart
+          xAxis={[{data: Array.from({ length: years[1] - years[0] + 1 }, (_, i) => years[0] + i), scaleType: "point"}]}
+          series={constructors}
+          width={800}
+          height={400}
+          sx = {() => ({
+            [`.${axisClasses.root}`]: {
+              [`.${axisClasses.tick}, .${axisClasses.line}`]: {
+                stroke: 'white',
+                strokeWidth: 3,
+              },
+              [`.${axisClasses.tickLabel}`]: {
+                fill: 'white',
+              },
+            },
+          })}
+          />
+        )
+      }
 
     </div>
   );
--- src/app/api/constructors/route.ts
+++ src/app/api/constructors/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient()
@@ -6,8 +6,25 @@ const prisma = new PrismaClient()
 export async function GET(req: NextRequest, res: NextResponse) {
     const url = new URL(req.url);
     const year = url.searchParams.get('year');
+    const years = year ? year.split(',').map(Number) : [];
+    let lowerBound: number | undefined;
+    let upperBound: number | undefined;
+    if (year) {
+      const yearRange = year.split(',').map(Number).sort((a, b) => a - b); 
+      lowerBound = yearRange[0];
+      upperBound = yearRange[1] || lowerBound; 
+  }
     //const items = await prisma.$queryRaw<{ id: string, name: string}[]>`SELECT id, name FROM constructor`;
-    const items = await prisma.$queryRaw<{ constructor_id: string}[]>`SELECT constructor_id, total_points, year FROM season_constructor WHERE year=${year}`;
+    //const items = await prisma.$queryRaw<{ constructor_id: string}[]>`SELECT constructor_id, total_points, year FROM season_constructor WHERE year=${year}`;
+    //const items = await prisma.$queryRaw<{ constructor_id: string, total_points: string, year: number }[]>`
+    //SELECT constructor_id, total_points, year 
+    //FROM season_constructor 
+    //WHERE year IN (${Prisma.join(years)})
+    //`;
+    const items = await prisma.$queryRaw<{ constructor_id: string, total_points: string, year: number }[]>`
+    SELECT constructor_id, total_points, year 
+    FROM season_constructor 
+    WHERE year BETWEEN ${lowerBound} AND ${upperBound}`;
 
     return new Response(JSON.stringify(items), {
       headers: { "Content-Type": "application/json" },
