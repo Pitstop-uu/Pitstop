877ae0e349a51d45e7c1d2e431b3abbcdced1f8c
EdvinEr <edvin.eriksson.6094@student.uu.se>
2024-11-22 10:37:07 +0100

    Fix deprecated onLoad

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ import "@/styles/hero.css";
 
 const Hero = () => (
     <div className="hero">
-      <Image src={hero} alt="Banner" layout="fill" quality={100} sizes="100vw" objectFit="cover" className="heroImage opacity-0" onLoadingComplete={(image => image.classList.remove("opacity-0"))}/>
+      <Image src={hero} alt="Banner" quality={100} sizes="100vw" className="heroImage opacity-0" onLoad={(e) => (e.target as HTMLImageElement).classList.remove("opacity-0")}/>
         <div className="logoWrapper">
           <Image quality={100} src={logo} alt="Logo" width={200} height={100} />
         </div>
