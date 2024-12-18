b1446ef243bba0851a47751e16fae9c0e2420bcf
wibr7068 <71329264+wibr7068@users.noreply.github.com>
2024-12-18 15:42:45 +0100

    Fixed ESLint errors

--- src/components/DropDownFilterMultiple.tsx
+++ src/components/DropDownFilterMultiple.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 
 import { Button } from "@/components/ui/button"
-import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
 interface DropDownFilterMultipleProps {
   list: KeyValue[];
